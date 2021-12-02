d5173e2966fcc89613ec3abc2a85c8cb0d7ea871
haixiangyan <haixiang6123@gmail.com>
2021-12-02 15:41:14 +0800

    feat: 添加主逻辑

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { createJsSdk } from '@/lib'
-import { invokeResMock, wxResMock } from '@/mock'
+import { checkRedirect, createJsSdk, initSdk } from '@/lib'
+import { invokeResMock, mockUserId, wxResMock } from '@/mock'
+import config from '@/_config'
+import { fetchSignatures, fetchUserId } from '@/api'
 
 Vue.config.productionTip = false
 
 export const jsSdk = createJsSdk(wxResMock, invokeResMock)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+checkRedirect(config, fetchUserId, mockUserId) // 重定向获取 code（用户身份）
+  .then(() => initSdk(jsSdk, config, fetchSignatures)) // 初始化 JsSdk
+  .then(() => {
+    new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  })
